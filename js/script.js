9a715826c271f1d0a151cf486a1e723b243a5f98
Lara <volynkinala@gmail.com>
2022-01-04 11:57:23 +0300

    catalog

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron_left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron_right.png"></button>'
+    });
+  });
