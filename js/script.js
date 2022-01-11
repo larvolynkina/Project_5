ecdf08dc88bbbbcb2120546f5e2264b05a5abaf4
Lara <volynkinala@gmail.com>
2022-01-11 20:13:17 +0300

    script

--- js/script.js
+++ js/script.js
@@ -26,6 +26,22 @@ $(document).ready(function(){
               $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
           })
     })
+
+    // Modal
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn();
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thankyou').fadeOut();
+    });
+
+    $('.catalog-item__btn').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn();
+        })
+    });
   });
 
   
